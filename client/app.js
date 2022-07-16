c63b15f6437e8335bdfc9f0522cf5d0e9802f34b
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-16 20:04:13 +0200

    Change userName from const to let, add function login, add form event listener

--- client/app.js
+++ client/app.js
@@ -5,4 +5,17 @@ const addMessageForm = document.getElementById("add-messages-form");
 const userNameInput = document.getElementById("username");
 const messageContentInput = document.getElementById("message-content");
 
-const userName = "";
+let userName = "";
+
+const login = (e) => {
+  e.preventDefault();
+  if (userNameInput.value !== "") {
+    userName = userNameInput.value;
+    loginForm.classList.toggle("show");
+    messageSection.classList.toggle("show");
+  } else {
+    alert("This field cannot be empty!");
+  }
+};
+
+loginForm.addEventListener("submit", (e) => login(e));
