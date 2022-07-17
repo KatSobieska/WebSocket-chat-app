b60a2921a1b10f27a90e90b98ad180b1bc7cf439
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-17 11:07:37 +0200

    Add information about user joined and user left

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/client")));
 
 let messages = [];
+let users = [];
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/index.html"));
@@ -17,14 +18,32 @@ const server = app.listen(8000, () => {
 });
 
 const io = socket(server);
+
 io.on("connection", (socket) => {
   console.log("New client! Its id - " + socket.id);
+  socket.on("join", (userName) => {
+    users.push({ id: socket.id, name: userName });
+    socket.broadcast.emit("message", {
+      author: "Chat Bot",
+      content: `<i>${userName} has joined the conversation!`,
+    });
+  });
+
   socket.on("message", (message) => {
     console.log("Oh, I've got something from " + socket.id);
     messages.push(message);
     socket.broadcast.emit("message", message);
   });
   socket.on("disconnect", () => {
+    if (users.length > 0) {
+      const user = users.find((user) => user.id === socket.id);
+      users.splice(users.indexOf(user), 1);
+      socket.broadcast.emit("message", {
+        author: "Chat Bot",
+        content: `<i>${user.name} has left the conversation... :(!`,
+      });
+    }
+
     console.log("Oh, socket " + socket.id + " has left");
   });
   console.log("I've added a listener on message event \n");
